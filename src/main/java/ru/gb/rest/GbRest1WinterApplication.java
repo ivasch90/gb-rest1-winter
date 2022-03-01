d2890ab41b91b9f1552da15dea8e16cbbd1cfea8
ivasch <melmett@yandex.ru>
2022-03-01 19:31:12 +0300

    Initial commit

---
+++ src/main/java/ru/gb/rest/GbRest1WinterApplication.java
@@ -0,0 +1,13 @@
+package ru.gb.rest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GbRest1WinterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GbRest1WinterApplication.class, args);
+	}
+
+}
---
+++ src/main/java/ru/gb/rest/dto/ProductDto.java
@@ -0,0 +1,34 @@
+package ru.gb.rest.dto;
+
+
+import lombok.*;
+import ru.gb.rest.entity.enums.Status;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.validation.constraints.*;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ProductDto {
+
+    private Long id;
+    @NotBlank(message = "Title is not required")
+    private String title;
+    @NotNull
+    @DecimalMin(value = "0.0", inclusive = false)
+    @Digits(integer = 5, fraction = 2)
+    private BigDecimal cost;
+    @PastOrPresent
+    private LocalDate date;
+    @NotNull
+    private Status status;
+
+
+}
